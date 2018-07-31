d7bf47ef3cd150bbcc5c9510d129e46cfe647817
visa111 <visaruttor@gmail.com>
2018-07-31 16:55:27 +0700

    Update newbotpush.php

--- newbotpush.php
+++ newbotpush.php
@@ -33,6 +33,41 @@ if($message == "นับ 1-10"){
           pushMsg($arrayHeader,$arrayPostData);
        }
     }
+////////////////////// check file
+
+$typeMessage = $events['events'][0]['message']['type']; 
+$idMessage = $events['events'][0]['message']['id']; 
+
+switch ($typeMessage){
+case (preg_match('/[image|audio|video]/',$typeMessage) ? true : false) :
+   $response = $bot->getMessageContent($idMessage);
+      if ($response->isSucceeded()) {
+        // คำสั่ง getRawBody() ในกรณีนี้ จะได้ข้อมูลส่งกลับมาเป็น binary 
+        // เราสามารถเอาข้อมูลไปบันทึกเป็นไฟล์ได้
+        $dataBinary = $response->getRawBody(); // return binary
+        // ทดสอบดูค่าของ header ด้วยคำสั่ง getHeaders()
+        $dataHeader = $response->getHeaders();   
+        $replyData = new TextMessageBuilder(json_encode($dataHeader));
+         
+          $arrayPostData['to'] = $id;
+          $arrayPostData['messages'][0]['type'] = "text";
+          $arrayPostData['messages'][0]['text'] = $response;
+          pushMsg($arrayHeader,$arrayPostData);
+         
+        break;
+    }
+    $failMessage = json_encode($idMessage.' '.$response->getHTTPStatus() . ' ' . $response->getRawBody());
+    $replyData = new TextMessageBuilder($failMessage);  
+      
+          $arrayPostData['to'] = $id;
+          $arrayPostData['messages'][0]['type'] = "text";
+          $arrayPostData['messages'][0]['text'] = "Error";
+          pushMsg($arrayHeader,$arrayPostData);
+    break;
+}
+
+////////////////////// check file
+
 
 
 
